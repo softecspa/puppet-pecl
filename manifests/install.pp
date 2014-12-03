66a1148ae5f953df2ece3a54987fade908df0a73
fpizzurro <felice.pizzurro@softecspa.it>
2014-12-03 10:20:34 +0000

    usa il modulo softec_php

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,17 +1,13 @@
 class pecl::install {
 
-  if !defined(Class['devutils']) {
-    include devutils
-  }
-  if !defined(Php5::Module['dev']) {
-    php5::module { 'dev': 
-      ensure  => 'pin'
-    }
+  include devutils
+
+  if !defined(Softec_php::Extension['dev']) {
+    softec_php::extension { 'dev':}
   }
 
-  php5::module {'php-pear':
-    ensure      => 'pin',
-    modulename  => 'php-pear',
-    require     => [ Class["php5::common"], Class["devutils"] ]
+  softec_php::extension {'php-pear':
+    package => 'php-pear',
+    require => Class["devutils"]
   }
 }
